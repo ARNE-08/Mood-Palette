ef2aeb1c72be9b6df58fde77dd1b9276570c7deb
tartartar <112954170+itsChewin@users.noreply.github.com>
2024-05-08 11:29:59 +0700

    [EDIT] Update Stat page

--- mood_palette/lib/screen/statistic/stat.dart
+++ mood_palette/lib/screen/statistic/stat.dart
@@ -26,24 +26,25 @@ class _StatPageState extends State<StatPage> {
     'December'
   ];
 
+  final Map<String, double> dataMap = {
+    "Angry": 3,
+    "Excited": 1,
+    "Happy": 2,
+    "Uncomfortable": 3,
+    "Confused": 4,
+    "Chill": 5,
+    "Calm": 6,
+    "Embarrassed": 7,
+    "Bored": 8,
+    "Sad": 11,
+    "Worried": 10,
+  };
+
   @override
   Widget build(BuildContext context) {
-    final Map<String, double> dataMap = {
-      "Angry": 3,
-      "Excited": 1,
-      "Happy": 2,
-      "Uncomfortable": 3,
-      "Confused": 4,
-      "Chill": 5,
-      "Calm": 6,
-      "Embarrassed": 7,
-      "Bored": 8,
-      "Sad": 11,
-      "Worried": 10,
-    };
-
     double sum = dataMap.values.reduce((value, element) => value + element);
-    final normalizedDataMap = dataMap.map((key, value) => MapEntry(key, (value / sum) * 100));
+    final normalizedDataMap =
+        dataMap.map((key, value) => MapEntry(key, (value / sum) * 100));
 
     final List<Color> colorList = [
       const Color.fromRGBO(255, 0, 34, 1),
@@ -60,35 +61,26 @@ class _StatPageState extends State<StatPage> {
     ];
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromRGBO(255, 254, 234, 1), // Set background color to match the container
-        elevation: 0, // Remove the elevation
-        title: Padding(
-          padding: const EdgeInsets.only(
-              top: 30), // Adjust top padding to move the text down
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'MoodPalette',
-                style: GoogleFonts.singleDay(
-                  textStyle: TextStyle(
-                    fontSize: 36,
-                  ),
-                ),
-              ),
-              SizedBox(width: 8),
-              Icon(Icons.calendar_today), // Calendar icon
-            ],
-          ),
-        ),
-      ),
-      body: Container(
-        color: Color.fromRGBO(255, 254, 234, 1), // Set background color here
-        width: double.infinity,
-        height: double.infinity,
+      backgroundColor: Color.fromRGBO(255, 254, 234, 1),
+      body: SingleChildScrollView(
         child: Column(
           children: [
+            SizedBox(height: 90),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'MoodPalette',
+                  style: GoogleFonts.singleDay(
+                    textStyle: TextStyle(
+                      fontSize: 36,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8),
+                Icon(Icons.calendar_today),
+              ],
+            ),
             SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -96,7 +88,8 @@ class _StatPageState extends State<StatPage> {
                 IconButton(
                   onPressed: () {
                     setState(() {
-                      currentMonthIndex = (currentMonthIndex - 1) % months.length;
+                      currentMonthIndex =
+                          (currentMonthIndex - 1) % months.length;
                     });
                   },
                   icon: Icon(Icons.arrow_back_ios),
@@ -108,7 +101,8 @@ class _StatPageState extends State<StatPage> {
                 IconButton(
                   onPressed: () {
                     setState(() {
-                      currentMonthIndex = (currentMonthIndex + 1) % months.length;
+                      currentMonthIndex =
+                          (currentMonthIndex + 1) % months.length;
                     });
                   },
                   icon: Icon(Icons.arrow_forward_ios),
@@ -119,7 +113,8 @@ class _StatPageState extends State<StatPage> {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                padding: EdgeInsets.symmetric(
+                    horizontal: 20, vertical: 0), // Adjust padding here
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(20),
@@ -128,48 +123,82 @@ class _StatPageState extends State<StatPage> {
                       color: Colors.grey.withOpacity(0.5),
                       blurRadius: 7,
                       spreadRadius: 0,
-                      offset: Offset(0, 10), // Bottom shadow
+                      offset: Offset(0, 10),
                     ),
                   ],
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(30),
-                  child: AspectRatio(
-                    aspectRatio: 0.6,
-                    child: PieChart(
-                      dataMap: normalizedDataMap,
-                      animationDuration: Duration(milliseconds: 800),
-                      chartLegendSpacing: 50,
-                      chartRadius: MediaQuery.of(context).size.width / 2,
-                      colorList: colorList,
-                      initialAngleInDegree: 0,
-                      chartType: ChartType.ring,
-                      ringStrokeWidth: 70,
-                      chartValuesOptions: ChartValuesOptions(
-                        showChartValues: true,
-                        showChartValuesInPercentage: true,
-                        showChartValueBackground: false,
-                        showChartValuesOutside: true,
-                        decimalPlaces: 0,
-                        chartValueStyle: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: GoogleFonts.poppins().fontFamily,
+                child: Column(
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 0.6,
+                      child: PieChart(
+                        dataMap: normalizedDataMap,
+                        animationDuration: Duration(milliseconds: 800),
+                        chartLegendSpacing: 10,
+                        chartRadius: MediaQuery.of(context).size.width / 2,
+                        colorList: colorList,
+                        initialAngleInDegree: 0,
+                        chartType: ChartType.ring,
+                        ringStrokeWidth: 70,
+                        chartValuesOptions: ChartValuesOptions(
+                          showChartValues: true,
+                          showChartValuesInPercentage: true,
+                          showChartValueBackground: false,
+                          showChartValuesOutside: true,
+                          decimalPlaces: 0,
+                          chartValueStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: GoogleFonts.poppins().fontFamily,
+                          ),
                         ),
-                      ),
-                      legendOptions: LegendOptions(
-                        showLegends: true,
-                        legendPosition: LegendPosition.bottom,
-                        legendShape: BoxShape.circle,
-                        legendTextStyle: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontSize: 12,
-                          fontFamily: GoogleFonts.poppins().fontFamily,
+                        legendOptions: LegendOptions(
+                          showLegends: false,
+                          legendPosition: LegendPosition.bottom,
+                          legendShape: BoxShape.circle,
+                          legendTextStyle: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 12,
+                            fontFamily: GoogleFonts.poppins().fontFamily,
+                          ),
                         ),
                       ),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: normalizedDataMap.entries
+                            .map(
+                              (entry) => Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 4.0),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: 20,
+                                      height: 20,
+                                      color: colorList[normalizedDataMap.keys
+                                          .toList()
+                                          .indexOf(entry.key)],
+                                    ),
+                                    SizedBox(width: 8),
+                                    Text(
+                                      '${entry.key}: ${entry.value.toStringAsFixed(1)}%',
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
