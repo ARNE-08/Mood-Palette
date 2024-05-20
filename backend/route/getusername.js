a2a05e682f4d3ebd2bc647d82ac74cffd21c34aa
Paramita Saenghao <judge.27miyoo@gmail.com>
2024-05-20 14:01:11 +0700

    [ADD] 2 new endpoints

---
+++ backend/route/getusername.js
@@ -0,0 +1,32 @@
+const mysql = require("mysql");
+const jwt = require("jsonwebtoken");
+
+module.exports = async (req, res) => {
+    try {
+        const token = req.body.user_id;
+        var decoded = jwt.verify(token, "ZJGX1QL7ri6BGJWj3t");
+        const user_id = decoded.userId;
+
+        const getUser = mysql.format("SELECT username FROM user WHERE user_id = ?", [user_id]);
+        connection.query(getUser, (err, rows) => {
+            if (err) {
+                return res.status(400).json({
+                    success: false,
+                    data: null,
+                    error: err.message,
+                });
+            }
+
+            return res.status(200).json({
+                success: true,
+                data: rows,
+            });
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            data: null,
+            error: 'Internal server error',
+        });
+    }
+};
